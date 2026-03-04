117b97476c6a99b7c95b2c33f07140d52f6d26f4
Krishnav Mahajan <krisnhav5555@gmail.com>
2026-03-05 00:20:10 +0530

    Restructure for Vercel serverless API routing

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 3001;
 
 app.use(express.json());
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Initialize Upstash Redis Client (Serverless HTTP)
 let upstashUrl = process.env.UPSTASH_REDIS_REST_URL || 'http://localhost:8079';
